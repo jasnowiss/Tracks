68e10dda5701c1b001738007f4388df94033c83d
danieljpark <kei99050@gmail.com>
2014-03-14 14:02:01 -0700

    Added Dan's upload tests

--- Project/Tracks/tests.py
+++ Project/Tracks/tests.py
@@ -27,10 +27,40 @@ class TrackTest(TestCase):
 class CollaborationTest(TestCase):
 
 
+"""
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
+
+Replace this with more appropriate tests for your application.
+"""
+
+from django.test import TestCase
+from Tracks.models import *
+
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+
+class UserProfileTest(TestCase):
+
+
+
+class TrackTest(TestCase):
+
+
+
+class CollaborationTest(TestCase):
+	
+
+
 class UploadFileTest(TestCase):
 
 	def test_valid_mp3_upload(self):
-		filepathOfMp3 = '~/mp3Files/valid.mp3'
+		filepathOfMp3 = '~/user_mp3_files/valid.mp3'
 		#make a request with this file path
 		request = 
 		self.assertEqual(self.upload_mp3(request), 'success')
@@ -40,32 +70,32 @@ class UploadFileTest(TestCase):
 
 
 	def test_invalid_file_extension(self):
-		filepathOfMp3 = '~/mp3Files/badExt.ogg'
+		filepathOfMp3 = '~/user_mp3_files/badExt.ogg'
 		#make a request with this file path
 		request = 
 		self.assertEqual(self.upload_mp3(request), 'File extension not supported')		
 
 
 	def test_sizeLimitExceeded_file(self):
-		filepathOfMp3 = '~/mp3Files/long.mp3'
+		filepathOfMp3 = '~/user_mp3_files/long.mp3'
 		#make a request with this file path
 		request = 
 		self.assertEqual(self.upload_mp3(request), 'File exceeding size limit')
 
 
 	def test_null_name(self):
-		filepathOfMp3 = '~/mp3Files/ .mp3'
+		filepathOfMp3 = '~/user_mp3_files/ .mp3'
 		#make a request with this file path
 		request = 
 		self.assertEqual(self.upload_mp3(request), 'form not valid')
 
 	def test_filenameTooLong_file(self):
-		filepathOfMp3 = '~/mp3Files/qweirouqwepiorqwpjfslfj asjkdfhsjkfhiwqehr quiwerh qwiuerh qwuier hqwuierh qwic vhmc hvmc hvcm hv qweruqiwerhqwouiehfskvnkjsadfbkjsadfhjsfhsjkdf wqierhwiuerhqwuierhw uifahsd fuahsdfiu sfhuiasd fhuiasdfhuiasd fhisahfisd fhuias dfhiasf iasdfaidfsadfhasd.mp3'
+		filepathOfMp3 = '~/user_mp3_files/qweirouqwepiorqwpjfslfj asjkdfhsjkfhiwqehr quiwerh qwiuerh qwuier hqwuierh qwic vhmc hvmc hvcm hv qweruqiwerhqwouiehfskvnkjsadfbkjsadfhjsfhsjkdf wqierhwiuerhqwuierhw uifahsd fuahsdfiu sfhuiasd fhuiasdfhuiasd fhisahfisd fhuias dfhiasf iasdfaidfsadfhasd.mp3'
 		request = 
 		self.assertEqual(self.upload_mp3(request), 'form not valid')
 	
 	def test_invalidChars_file(self):
-		filepathOfMp3 = '~/mp3Files/@#ds!fd.mp3'
+		filepathOfMp3 = '~/user_mp3_files/@#ds!fd.mp3'
 		#make a request with this file path
 		request = 
 		self.assertEqual(self.upload_mp3(request), 'form not valid')

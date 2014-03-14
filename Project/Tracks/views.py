45fcddc9fe91ed10c2c85ee2a04006ed29c62e4e
alexthoklesample <alexthoklesample@gmail.com>
2014-03-13 17:40:32 -0700

    don't commit

--- Project/Tracks/views.py
+++ Project/Tracks/views.py
@@ -15,6 +15,7 @@ from Tracks.models import Track
 import os
 import sys
 import traceback
+from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
@@ -27,7 +28,6 @@ def register(request):
     pass
 
 
-""" # Julian's version, undo when ready
 def signIn(request):
     # Custom login
     email = password = ''
@@ -40,22 +40,22 @@ def signIn(request):
             if user.is_active:
                 login(request, user)
                 return HttpResponseRedirect('')
+    if (request.method == 'GET'):
+        return render(request, 'Tracks/welcome.html',{})
 
     return render_to_response('login.html', {'email': email})
-"""
 
 
 def tracks(request):
     return render(request, 'Tracks/tracks.html',{})
+    
 
-
-def signIn(request):
-    if (request.method == 'GET'):
-        return render(request, 'Tracks/welcome.html',{})
-
-
+@csrf_exempt
 def signUp(request):
     if (request.method == 'GET'):
+        return render(request, 'Tracks/signup.html', {})
+    else:
+        print request
         return render(request, 'Tracks/signup.html', {})
 
 

9bdd1cd3d27cfa062e0d08a71eb4565e893e4535
alexthoklesample <alexthoklesample@gmail.com>
2014-04-18 13:59:38 -0700

    waveform editing!

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -91,6 +91,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'Project.django-crossdomainxhr-middleware.XsSharing',
 )
 
 ROOT_URLCONF = 'Project.urls'

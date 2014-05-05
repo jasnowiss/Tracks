74fadfde8db4341bea49f69fd65b0fc3daeaaa28
jasnowiss <jasnowiss@gmail.com>
2014-05-05 14:13:14 -0700

    my django app 9.5

--- Project/settings.py
+++ Project/settings.py
@@ -174,7 +174,7 @@ MEDIA_URL = 'http://127.0.0.1:8000/Tracks/user_mp3_files/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
--- Tracks/urls.py
+++ Tracks/urls.py
@@ -48,10 +48,13 @@ urlpatterns = patterns('',
 if settings.DEBUG:
     urlpatterns += patterns('',
         url(r'^user_mp3_files/(?P<path>.*)$', views.play_MP3),
-        url(r'^resetFixture/$', views.resetFixture)
+        url(r'^resetFixture/$', views.resetFixture),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
    )
 
+"""
 if not settings.DEBUG:
     urlpatterns += patterns('',
         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     )
+"""

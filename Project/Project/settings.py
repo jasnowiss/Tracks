14a2d25641bc1e97aec9dd2f99de718f0b67bd9d
jasnowiss <jasnowiss@gmail.com>
2014-05-05 12:50:51 -0700

    my django app

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -12,9 +12,15 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 Build Paths
 """
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+"""
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # BASE_DIR = '..\\Tracks'
+"""
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 
@@ -44,12 +50,14 @@ Valid Hosts/Domain Names
 """
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
 """
 Database
+"""
+
 """
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 DATABASES = {
@@ -63,7 +71,11 @@ DATABASES = {
         'PORT': '',         # might be able to comment out             # Set to empty string for default.
     }
 }
+"""
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
 
 
 """
@@ -164,7 +176,7 @@ MEDIA_URL = 'http://127.0.0.1:8000/Tracks/user_mp3_files/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -175,6 +187,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -224,3 +237,6 @@ LOGGING = {
 }
 
 LOGIN_URL = '/Tracks/signin/'
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
